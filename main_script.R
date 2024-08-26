983ac23a68d110e65aabc8d70204a56fed5e312a
lucarenz1997 <luca.renz@hotmail.com>
2024-08-26 14:22:32 +0200

    Filter for dates newer than 2015

--- main_script.R
+++ main_script.R
@@ -1,11 +1,5 @@
 df <- read.csv("datasets/Motor_Vehicle_Collisions_-_Crashes_20240826.csv", header=TRUE)
-colnames(df)
-df$CRASH.DATE
-df$CRASH.TIME
-
-# Assuming DataFrame A is named df_A and DataFrame B is named df_B
-
-# Convert crash.date and crash.time to a POSIXct datetime object in df_A
+head(df)
 df$datetime <- as.POSIXct(paste(df$crash.date, df$crash.time), format = "%m/%d/%Y %H:%M")
 
 
