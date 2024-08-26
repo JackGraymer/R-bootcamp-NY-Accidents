3823d0b0a6b557edab69aa46095ff71854ac0212
lucarenz1997 <luca.renz@hotmail.com>
2024-08-26 16:53:25 +0200

    Add additional column 'Category'

--- main_script.R
+++ main_script.R
@@ -1,12 +1,12 @@
 ## packages
 
-install.packages("lubridate")
+# install.packages("lubridate")
 library(lubridate) # used to round to the next hour
 
-install.packages("dplyr")
+# install.packages("dplyr")
 library(dplyr)
 
-install.packages("readxl")
+# install.packages("readxl")
 library(readxl)
 
 # VEHICLES DF - CLEANING
@@ -27,13 +27,55 @@ columns_to_remove <- c(
 vehicles_df <- vehicles_df %>%
   select(-all_of(columns_to_remove))
 
-# Filter the dataframe
-vehicles_df <- vehicles_df[vehicles_df$CRASH.DATE >= cutoff_date, ]
-glimpse(vehicles_df)
+
+# Create broader categories for main causes
+# Your list of strings
+conditions <- c("Aggressive Driving/Road Rage", "Pavement Slippery", "Following Too Closely", 
+                "Unspecified", "", "Passing Too Closely", "Driver Inexperience", 
+                "Passing or Lane Usage Improper", "Turning Improperly", "Unsafe Lane Changing", 
+                "Unsafe Speed", "Reaction to Uninvolved Vehicle", "Steering Failure", 
+                "Traffic Control Disregarded", "Other Vehicular", "Driver Inattention/Distraction", 
+                "Oversized Vehicle", "Pedestrian/Bicyclist/Other Pedestrian Error/Confusion", 
+                "Alcohol Involvement", "View Obstructed/Limited", "Failure to Yield Right-of-Way", 
+                "Illnes", "Lost Consciousness", "Brakes Defective", "Backing Unsafely", "Glare", 
+                "Passenger Distraction", "Fell Asleep", "Obstruction/Debris", "Tinted Windows", 
+                "Animals Action", "Drugs (illegal)", "Pavement Defective", "Other Lighting Defects", 
+                "Outside Car Distraction", "Driverless/Runaway Vehicle", "Tire Failure/Inadequate", 
+                "Fatigued/Drowsy", "Headlights Defective", "Accelerator Defective", 
+                "Failure to Keep Right", "Physical Disability", "Eating or Drinking", 
+                "Cell Phone (hands-free)", "Lane Marking Improper/Inadequate", 
+                "Cell Phone (hand-Held)", "Using On Board Navigation Device", "Other Electronic Device", 
+                "Traffic Control Device Improper/Non-Working", "Tow Hitch Defective", 
+                "Windshield Inadequate", "Vehicle Vandalism", "Shoulders Defective/Improper", 
+                "Prescription Medication", "Listening/Using Headphones", "Texting", "80", 
+                "Reaction to Other Uninvolved Vehicle", "1", "Drugs (Illegal)", "Illness", 
+                "Cell Phone (hand-held)")
+
+# Create categorizatoin/mapping for conditions
+categorize_condition <- function(condition) {
+  if (grepl("Driving|Following|Passing|Inexperience|Improper|Changing|Speed|Distraction|Alcohol|Drugs|Phone|Eating|Fatigued|Sleeping|Pedestrian|Failure to Yield|Backing|Oversized", condition, ignore.case = TRUE)) {
+    return("Human Error")
+  } else if (grepl("Brakes|Steering|Tire|Headlights|Accelerator|Windshield|Tow Hitch|Defective|Failure", condition, ignore.case = TRUE)) {
+    return("Mechanical Error")
+  } else if (grepl("Pavement|Glare|Obstruction|Weather|Animals|View|Control|Shoulders", condition, ignore.case = TRUE)) {
+    return("Environmental Conditions")
+  } else if (grepl("Illness|Lost Consciousness|Physical Disability", condition, ignore.case = TRUE)) {
+    return("Medical Condition")
+  } else if (condition == "" || grepl("Unspecified|Other", condition, ignore.case = TRUE)) {
+    return("Other/Unspecified")
+  } else {
+    return("Other/Unspecified")
+  }
+}
+
+
+# create new column with broader category for accident
+vehicles_df <- vehicles_df %>%
+  mutate(Category = sapply(CONTRIBUTING.FACTOR.VEHICLE.1, categorize_condition))
 
 # Define the cutoff date as other dataframe starts with 2016
 cutoff_date <- as.Date("2016-01-01")
-
+vehicles_df <- vehicles_df[vehicles_df$CRASH.DATE >= cutoff_date, ]
 vehicles_df <- vehicles_df %>%
   # Combine date and time into one string
   mutate(CRASH.TIME = as.POSIXct(paste(CRASH.DATE, CRASH.TIME), format = "%Y-%m-%d %H:%M")) %>%
@@ -41,8 +83,6 @@ vehicles_df <- vehicles_df %>%
   mutate(CRASH.TIME = ceiling_date(CRASH.TIME, "hour")) %>%
   # Create the final CRASH.DATETIME column
   mutate(CRASH.DATETIME = CRASH.TIME)
-
-# write.csv(vehicles_df,"vehicles_v2.csv")
 
 
 # WEATHER DF - CLEANING
@@ -57,10 +97,4 @@ glimpse(weather_df)
 # MERGING DATAFRAMES
 merged_df <- inner_join(vehicles_df, weather_df, by = c("CRASH.DATETIME" = "time"))
 
-
-
-
-
-
-
-
+write.csv(merged_df,"merged.csv")
