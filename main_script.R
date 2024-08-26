cf528fb88646cffb1685a886bd2eff7ca38446b6
lucarenz1997 <luca.renz@hotmail.com>
2024-08-26 14:36:40 +0200

    Add weather dataframe

--- main_script.R
+++ main_script.R
@@ -1,6 +1,24 @@
-df <- read.csv("datasets/Motor_Vehicle_Collisions_-_Crashes_20240826.csv", header=TRUE)
-head(df)
-df$datetime <- as.POSIXct(paste(df$crash.date, df$crash.time), format = "%m/%d/%Y %H:%M")
+# VEHICLES DF - CLEANING
+vehicles_df <- read.csv("data/vehicles.csv", header=TRUE)
+vehicles_df$CRASH.DATE <- as.Date(vehicles_df$CRASH.DATE, format = "%m/%d/%Y")
+
+# Define the cutoff date as other dataframe starts with 2016
+cutoff_date <- as.Date("2016-01-01")
+
+# Filter the dataframe
+vehicles_df <- vehicles_df[vehicles_df$CRASH.DATE >= cutoff_date, ]
+
+
+# WEATHER DF - CLEANING
+
+install.packages("readxl")
+library(readxl)
+weather_df <- read_excel("data/weather.xlsx")
+
+
+# MERGING DATAFRAMES
+
+
 
 
 
